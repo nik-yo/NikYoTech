97aac211a9ccf4baa030911e466b954003688d6d
Nikki Yodo <nik_yodo@yahoo.com>
2024-08-29 13:24:59 -0500

    update front page and services

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,31 @@
-import Button from "@/components/button";
-import LinkButton from "@/components/link-button";
-import ServiceCard from "@/components/service-card";
-import Link from "next/link";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import LinkButton from "@/components/link-button"
+import Link from "next/link"
+import { FaGithub, FaLinkedin } from "react-icons/fa"
+import ServiceData from "../data/services.json"
 
 export default function Home() {
-  const services = [
-    {
-      title: "Developing to completion",
-      desc: "In one of my projects, I took over mobile app development that has been ongoing for 9 months and I spent 3 months each to bring both the iOS and android apps to the market."
-    },
-    {
-      title: "Architect a solution",
-      desc: "Once, our cloud cost spiraled out of control and to solve the issue I architected a solution that doesn't involve code change and significant infrastructure change, and managed to save the company around $15,000 a month."
-    },
-    {
-      title: "Process automation",
-      desc: "Sometimes there are many tickets tied to a single Pull Request, so I created an extension that get triggered on Pull Request completion and automatically moved the tickets to the right location which saved a lot of manual work and time."
-    },
-    {
-      title: "Proof of concept",
-      desc: "I was asked if programmatically moving data from one EBS to a smaller EBS was feasible, so I did proof of concepts using various partition table, file systems, linux distros and windows versions. The solution handled most scenario, passed QA and made it to production."
-    },
-    {
-      title: "Problem solving and troubleshooting",
-      desc: "One day our application started crashing, unable to connect to our MySQL database. Some troubleshooting later, I found out that the new base container image has disabled old cipher suites and the database doesn't support any newer cipher suites. I ended up re-enabling the old cipher suites until the database was upgraded."
-    }
-  ]
+  // const services = [
+  //   {
+  //     title: "Developing to completion",
+  //     desc: "In one of my projects, I took over mobile app development that has been ongoing for 9 months and I spent 3 months each to bring both the iOS and android apps to the market."
+  //   },
+  //   {
+  //     title: "Architect a solution",
+  //     desc: "Once, our cloud cost spiraled out of control and to solve the issue I architected a solution that doesn't involve code change and significant infrastructure change, and managed to save the company around $15,000 a month."
+  //   },
+  //   {
+  //     title: "Process automation",
+  //     desc: "Sometimes there are many tickets tied to a single Pull Request, so I created an extension that get triggered on Pull Request completion and automatically moved the tickets to the right location which saved a lot of manual work and time."
+  //   },
+  //   {
+  //     title: "Proof of concept",
+  //     desc: "I was asked if programmatically moving data from one EBS to a smaller EBS was feasible, so I did proof of concepts using various partition table, file systems, linux distros and windows versions. The solution handled most scenario, passed QA and made it to production."
+  //   },
+  //   {
+  //     title: "Problem solving and troubleshooting",
+  //     desc: "One day our application started crashing, unable to connect to our MySQL database. Some troubleshooting later, I found out that the new base container image has disabled old cipher suites and the database doesn't support any newer cipher suites. I ended up re-enabling the old cipher suites until the database was upgraded."
+  //   }
+  // ]
 
   return (
     <>
@@ -42,17 +41,16 @@ export default function Home() {
         <Link className="text-blue-700 font-bold text-2xl" href="/projects">Check out my project page </Link>
       </div>
       <div className="my-2">
-        <h1 className="text-2xl font-bold">What values can I provide?</h1>
+        <h1 className="text-2xl font-bold">Services</h1>
       </div>
       <div className="flex flex-col space-y-4 my-2">
+        <ul className="list-disc px-4">
         {
-          services.map((s, i) => (
-            <ServiceCard key={`svc-${i}`}>
-              <h2 className="font-bold">{s.title}</h2>
-              <p>{s.desc}</p>
-            </ServiceCard>
+          ServiceData.map((s, i) => (
+            <li key={`svc-${i}`} className="py-2 text-xl">{s.name}</li>
           ))
         }
+        </ul>
       </div>
       <div className="text-center my-8">
         <div className="mb-4">Other needs? Let&apos;s discuss.</div>
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,33 +1,18 @@
+import ServiceData from "../../data/services.json"
+
 export default function Services() {
   return (
     <div className="my-4 space-y-2">
       <h1 className="text-2xl font-bold">Services</h1>
       <p>Some services that I provide</p>
-      <div className="py-4">
-        <h2 className="text-xl font-bold">Custom Application Development</h2>
-        <p>I can help with developing custom application and solution for web, mobile, and cloud.</p>
-      </div>
-      <div className="py-4">
-        <h2 className="text-xl font-bold">Software and Cloud Architecture</h2>
-        <p>A good architecture will speed development, ease maintenance, result in robust operation, stronger security, and lower financial cost. I can help with architecting one.</p>
-      </div>
-      <div className="py-4">
-        <h2 className="text-xl font-bold">Maintenance and Operation</h2>
-        <p>Leave maintenance and operations to me, so you can focus on your business.</p>
-      </div>
-      <div className="py-4">
-        <h2 className="text-xl font-bold">Automation and Optimization</h2>
-        <p>Leave things that required fast response and repetition to automation. I can also help with performance, cost, and process optimization.</p>
-      </div>
-      <div className="py-4">
-        <h2 className="text-xl font-bold">Analytics and Troubleshooting</h2>
-        <p>Going through data can be overwhelming. I can help with analytics of performance and data. Sometimes the more eyes the better, I can come along your team and help with troubleshooting.</p>
-      </div>
-      <div className="py-4">
-        <h2 className="text-xl font-bold">Research and Proof of Concept</h2>
-        <p>Have an idea but not sure if it will work? I can help with in-depth research and proof of concept.</p>
-      </div>
-      
+      {
+        ServiceData.map((s, i) => (
+          <div key={`svc-${i}`} className="py-4">
+            <h2 className="text-xl font-bold">{s.name}</h2>
+            <p>{s.desc}</p>
+          </div>
+        ))
+      }      
     </div>
   )
 }
