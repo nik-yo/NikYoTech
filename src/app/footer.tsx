279d4069e50a07bc6085a92ce19e9110af4b5dcc
Nikki Yodo <nik_yodo@yahoo.com>
2024-09-09 15:14:53 -0500

    add social media on footer

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,7 +1,17 @@
+import Content from "@/components/content";
+import Link from "next/link";
+import { FaGithub, FaLinkedin } from "react-icons/fa6";
+
 export default function Footer() {
   return (
-    <div className="py-3 px-8 md:px-32 lg:px-48 xl:px-64 2xl:px-96 bg-blue-500 text-white">
-      &copy; {new Date().getFullYear()} NikYo Technology Consulting LLC
-    </div>
+    <Content className="py-3 bg-blue-500 text-white flex items-center">
+      <div className="grow">
+        &copy; {new Date().getFullYear()} NikYo Technology Consulting LLC
+      </div>      
+      <div className="flex justify-end space-x-4">
+        <Link href="https://github.com/nik-yo"><FaGithub className="size-8" /></Link>
+        <Link href="https://www.linkedin.com/in/nikkiyodo"><FaLinkedin href="" className="size-8" /></Link>
+      </div>
+    </Content>
   )
 }
