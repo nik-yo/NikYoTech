65425a16b80d58037b5bb547c5434a4f13fb3cd2
Nikki Yodo <nik_yodo@yahoo.com>
2024-09-10 23:47:35 -0500

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,13 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./header"
 import Footer from "./footer";
+import GoogleAnalytics from '../components/google-analytics';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "NikYo Technology",
-  description: "Helping with technology",
+  description: "Help achieving business goals using technology",
 };
 
 export default function RootLayout({
@@ -18,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleAnalytics />
       <body className={`${inter.className} flex flex-col min-h-screen`}>
         <Header />
         <main className="flex flex-col grow">
