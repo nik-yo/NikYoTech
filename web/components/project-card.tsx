e2b6b9634c77f29556f15bad776bb51802c3abaa
Nikki Yodo <nik_yodo@yahoo.com>
2024-10-20 19:53:59 -0500

    fix null error

--- web/components/project-card.tsx
+++ web/components/project-card.tsx
@@ -3,7 +3,7 @@ export default function ProjectCard(props: { title?: string, desc?: string, stac
     <div className="p-4 rounded-md shadow border">
       <h2 className="text-lg font-bold">{props.title}</h2>
       <p>{props.desc}</p>
-      <p className="mt-2"><strong>Stack</strong>: {props.stack.join(', ')}</p>
+      <p className="mt-2"><strong>Stack</strong>: {props.stack?.join(', ')}</p>
     </div>
   )
 }
